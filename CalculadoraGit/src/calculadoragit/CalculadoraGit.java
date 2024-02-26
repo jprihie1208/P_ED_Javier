0599103d983ea2094fa0e1590e6a338342e9a42e
J <javier@example.com>
2024-02-26 02:13:05 +0100

    Sumar añadido

---
+++ CalculadoraGit/src/calculadoragit/CalculadoraGit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoragit;
+
+/**
+ *
+ * @author javip
+ */
+public class CalculadoraGit {
+
+    public static void main(String[] args) {
+        
+    }
+    
+    public static int sumar(int a, int b){
+        return a+b;
+    
+        
+    }
+    
+}
